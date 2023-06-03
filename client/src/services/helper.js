c4d1aab19c3fa77641adeed1316b6d4b3083e9b7
himangshumahato82 <himangshumahato82@gmail.com>
2023-06-03 20:50:27 +0530

    update

--- client/src/services/helper.js
+++ client/src/services/helper.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = "https://spiteful-animal-production.up.railway.app"
 
-export const BASE_URL = "http://localhost:6010"
+
+export const BASE_URL = "https://nyx-backend-api.onrender.com"
